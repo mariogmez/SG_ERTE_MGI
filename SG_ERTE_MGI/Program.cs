b313b858f47f631c6398827ec956ccf8c02260e3
mariogmez <mariogomex199921@gmail.com>
2022-05-02 10:03:27 +0200

    CMT 10:03 02/05

---
+++ SG_ERTE_MGI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SG_ERTE_MGI
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
